ff3caca093e4dda76f8145b53bc0843390d7d91e
Jason Butler <jbutler42@port2002.com>
2015-08-06 16:10:20 -0700

    renamed files

--- week2/exercise_2_and_3.py
+++ week2/exercise_2_and_3.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+'''
+2. telnetlib
+
+    a. Write a script that connects using telnet to the pynet-rtr1 router. Execute the 'show ip int brief' command on the router and return the output.
+
+Try to do this on your own (i.e. do not copy what I did previously). You should be able to do this by using the following items:
+
+telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+remote_conn.read_until(<string_pattern>, TELNET_TIMEOUT)
+remote_conn.read_very_eager()
+remote_conn.write(<command> + '\n')
+remote_conn.close()
+
+
+3. telnetlib (optional - challenge question)
+
+    Convert the code that I wrote here to a class-based solution (i.e. convert over from functions to a class with methods).
+'''
+
 from __future__ import print_function
 import telnetlib
 import time
